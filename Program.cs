6535a5922c50e0ae151dd5f22f3d8e77c4e6d096
Ay <ay.kerem@training.lug-ag.de>
2024-12-09 09:59:37 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Erstellen Sie einen Algorithmus der das Spiel FizzBuzz bis zur 100 durchführt.
+            //Regeln siehe https://de.wikipedia.org/wiki/Fizz_buzz.
+
+            //Modulo gibt uns den Rest einer Division als Ganzzahl zurück. Das Zeichen für den Operanten ist %
+            //Beispiel: 4 % 2 = 0
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("Fizzbuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
